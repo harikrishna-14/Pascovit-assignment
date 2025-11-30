3068df2ca54430c98c609929144a212637a33897
harikrishna-14 <harikrishna.rabbagundu@gmail.com>
2025-11-30 21:44:46 +0530

    Change API base URL to localhost for development

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: "https://pascovit-assignment.onrender.com",
+  baseURL: "http://localhost:5000",
   withCredentials: true,
 });
 
